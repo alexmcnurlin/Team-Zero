9253f0cd0e14faa6006922cd01b5c53fc193afa8
Jorge Olivas <japowins@gmail.com>
2018-02-19 16:53:33 -0800

    Added Snow and Grass
    Snow accelerates the player.
    Grass makes the speed normal again.

---
+++ Assets/Scripts/jo/NonCharacterCollisionController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonCharacterCollisionController:MonoBehaviour {
+
+    public enum TileTypes 
+     {
+        None,
+        DefaultSpeed,
+        Slowing,
+        Accelerating,
+        Damaging,
+        Killing,
+        EndOfLevel
+
+    }
+
+    public enum ItemTypes 
+    {
+        None,
+        Token,
+        Powerup
+    }
+
+    public TileTypes tileType;
+    public ItemTypes itemType;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        object[] tempStorage = new object[4];
+
+        if(tileType != TileTypes.None) {
+
+            switch(tileType) {
+                case TileTypes.DefaultSpeed:
+                    tempStorage[0] = "NormalSpeed";
+                    tempStorage[1] = 1f;
+                    other.gameObject.SendMessage("CollideWithObject", tempStorage);
+                    break;
+
+                case TileTypes.Slowing:
+                    tempStorage[0] = "Slowing";
+                    tempStorage[1] = 0.6f;
+                    other.gameObject.SendMessage("CollideWithObject", tempStorage);
+                    break;
+
+                case TileTypes.Accelerating:
+                    tempStorage[0] = "Accelerating";
+                    tempStorage[1] = 1.5f;
+                    other.gameObject.SendMessage("CollideWithObject", tempStorage);
+                    break;
+
+                case TileTypes.Damaging:
+                    tempStorage[0] = "Damaging";
+                    tempStorage[1] = 1; //should be a variable up there
+                    other.gameObject.SendMessage("CollideWithObject", tempStorage);
+                    break;
+
+                case TileTypes.Killing:
+                    other.gameObject.SendMessage("CollideWithObject", "Killing");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
