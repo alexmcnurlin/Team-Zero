c6f1d43c4fe120d1cb34eaf2ea8d41076db16e81
Josue Espinosa <josue@vynyl.com>
2018-02-19 17:12:02 -0800

    add FriendlyNPC class

---
+++ Assets/Scripts/je/FriendlyNPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlyNPC : MonoBehaviour {
+
+	private string[] dialogue;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void Chat () 
+	{
+
+	}
+
+	void AssignQuest (/*Quest quest*/) 
+	{
+
+	}
+}
