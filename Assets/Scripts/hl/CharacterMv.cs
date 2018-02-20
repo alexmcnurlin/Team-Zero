eb463b0ce71c8621898f980bb93506617d6108bb
Josue Espinosa <josue@vynyl.com>
2018-02-19 17:42:40 -0800

    proper merge?

--- Assets/Scripts/hl/CharacterMv.cs
+++ Assets/Scripts/hl/CharacterMv.cs
@@ -4,99 +4,83 @@ using UnityEngine;
 
 public class Player : Character {
 
-    private Rigidbody2D rb2d;
-    public int counter;
+	private Rigidbody2D rb2d;
+	public int counter;
 
-    // Use this for initialization
-    void Start ()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
 
-    private void FixedUpdate()
-    {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
+	private void FixedUpdate()
+	{
+		//Store the current horizontal input in the float moveHorizontal.
+		float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
+		//Store the current vertical input in the float moveVertical.
+		float moveVertical = Input.GetAxis("Vertical");
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		//Use the two store floats to create a new Vector2 variable movement.
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
-    }
+		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+		rb2d.AddForce(movement * speed);
+	}
 
-    // Update is called once per frame
-    void Update ()
-    {
-	
-    }
+	// Update is called once per frame
+	void Update ()
+	{
 
-    void ApplyPowerup(Powerup)
-    {
+	}
 
-    }
+	void ApplyPowerup()
+	{
 
-    void CollideWithSlowingObject()
-    {
+	}
 
-    }
+	void CollideWithObject(object tmp)
+	{
+		// look at Jorge's stuff
+	}
 
-    void CollideWtihAccelerationObject()
-    {
+	void CollideWithObject(string kill)
+	{
+		// look at Jorge's stuff
+	}
 
-    }
+	void CollideWithResettingObject()
+	{
 
-    void CollideWithKillingObject()
-    {
+	}
 
-    }
+	void CollideWithObjectSound()
+	{
 
-    void CollideWtihDamagingObject()
-    {
+	}
 
-    }
-    
-    void CollideWithResettingObject()
-    {
+	void OnTriggerEnter(Collider other)
+	{
 
-    }
+	}
 
-    void CollideWithObjectSound()
-    {
+	void SetScoreText()
+	{
 
-    }
+	}
 
-    void Player()
-    {
+	void SendPlayerScore()
+	{
 
-    }
+	}
 
-    void FixedUpdate()
-    {
+	void InteractsWithUI()
+	{
 
-    }
+	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        
-    }
+	override protected void OnCollision () {
 
-    void SetScoreText()
-    {
-
-    }
-
-    void SendPlayerScore()
-    {
-
-    }
-
-    void InteractsWithUI()
-    {
-
-    }
+	}
 
 }
