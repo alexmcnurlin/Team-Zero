6833a55cc26cf20aa58568c7405ecda72bab4bbe
Josue Espinosa <josue@vynyl.com>
2018-02-19 16:24:17 -0800

    enemy subclass

---
+++ Assets/Scripts/je/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character {
+
+	protected override void OnCollision () {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
