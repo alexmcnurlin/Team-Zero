7c65ef4f3bd0271ae7dc4f1861f0f98aa73520a8
Simon Barnes <simonisadude@yahoo.com>
2018-02-19 16:10:50 -0800

    audio manager skeleton

---
+++ Assets/Scripts/sb/audio.cs
@@ -0,0 +1,33 @@
+using System.Media;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	
+	private bool musicPlaying = false;
+	private bool fxPlaying = false;
+	private bool isMusted = false;
+	private bool isInit = false;
+	
+	
+	void Start ()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+	int PlayMusic(string music)
+	{
+		
+	}
+	
+	int PlayFx(string fx)
+	{
+		
+	}
+	
+}
