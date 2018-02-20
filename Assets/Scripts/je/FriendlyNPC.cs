51c748a2e51bd8afb5be39f0c1036b4b1e7a7da8
Josue Espinosa <josue@vynyl.com>
2018-02-19 17:31:27 -0800

    make npc move

--- Assets/Scripts/je/FriendlyNPC.cs
+++ Assets/Scripts/je/FriendlyNPC.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class FriendlyNPC : MonoBehaviour {
 
 	private string[] dialogue;
+	private float velocity = 1;
 
 	// Use this for initialization
 	void Start () 
 	{
 		
 	}
+
+	public void Move () 
+	{
+		Vector2 position = transform.position;
+		float translation = Time.deltaTime * velocity;
+		position.x += translation;
+		transform.position = position;
+	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		Move ();
 	}
 
 	void Chat () 
 	{
 
+	}
+
+	public void CollideWithObject(object[] temp) {
+		string type = (string) temp[0];
+		float speed = (float) temp[1];
+		velocity *= speed * 3;
 	}
 
 	void AssignQuest (/*Quest quest*/) 
