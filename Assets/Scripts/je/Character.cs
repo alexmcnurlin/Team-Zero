f24920fc27d2ebdb8839d45c7bc3f186c73ed1c1
Josue Espinosa <josue@vynyl.com>
2018-02-19 16:13:47 -0800

    initial version of Character base class

--- Assets/Scripts/je/Character.cs
+++ Assets/Scripts/je/Character.cs
@@ -3,40 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Character : MonoBehaviour {
+
+	public enum Direction {
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	}
 	
-	public abstract void Move ();
+	public void Move ();
+	public bool IsDead ();
+	public int GetHealth();
+	public int GetSpeed();
+	public Direction GetDirection();
+	public void UpdateHealth (int newHealth);
+	public void UpdateSpeed (int newSpeed);
+	public void ChangeDirection (Direction newDirection);
 
 	protected abstract void OnCollision ();
 
 	// private list inventory;
 
+	private int MAX_HEALTH;
+	private int MAX_SPEED;
+
 	private int money;
-	private double health;
-	private double speed;
-	// TODO: enum LEFT, RIGHT, UP, DOWN
-	private int direction;
+	private int health;
+	private int velocity;
+	private int speed;
+	private Direction direction;
 
 	Character (double velocity) {
 
 	}
 
-//	public abstract void move() {
-		// component.getposition + 1
-		// TODO: use delta time
-//	}
-
-//	protected abstract void onCollision() {
-		// 
-//	}
-
-	// Use this for initialization
-	void Start () {
-		
+	public bool IsDead() {
+		return (health <= 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		// get all items in hitbox of self, typecast to strictly character
-		// if true, collision occurred, deal w it
+
+	public int GetHealth() {
+		return health;
+	}
+
+	public int GetSpeed() {
+		return speed;
+	}
+
+	public Direction GetDirection() {
+		return direction;
+	}
+
+	public void UpdateHealth (int newHealth) {
+		health = newHealth;
+	}
+
+	public void UpdateSpeed (int newHealth) {
+		health = newHealth;
+	}
+
+	public void ChangeDirection (Direction newDirection) {
+		direction = newDirection;
 	}
 }
