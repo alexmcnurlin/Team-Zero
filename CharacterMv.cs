c023f015019847262d6b3690845064b8c8bec9dd
Hayden <lepl1529@vandals.uidaho.edu>
2018-02-19 16:40:15 -0800

    Add files via upload

--- CharacterMv.cs
+++ CharacterMv.cs
@@ -6,17 +6,26 @@ public class Player : Character {
 
     private Rigidbody2D rb2d;
     public int counter;
-    private Scoretext scoretext;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 
     private void FixedUpdate()
-    { 
-        
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
     }
 
     // Update is called once per frame
@@ -89,4 +98,5 @@ public class Player : Character {
     {
 
     }
+
 }
