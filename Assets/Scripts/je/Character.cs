5ba1f62946bc5f2728a00d5ddf14cc6a471b1add
Josue Espinosa <josue@vynyl.com>
2018-02-19 16:31:51 -0800

    use allman brackets on character subclass

--- Assets/Scripts/je/Character.cs
+++ Assets/Scripts/je/Character.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public abstract class Character : MonoBehaviour {
 
-	public enum Direction {
+	public enum Direction 
+	{
 		LEFT,
 		RIGHT,
 		UP,
@@ -24,35 +25,43 @@ public abstract class Character : MonoBehaviour {
 	private int speed;
 	private Direction direction;
 
-	public void Move() {
+	public void Move () 
+	{
 
 	}
 
-	public bool IsDead() {
+	public bool IsDead () 
+	{
 		return (health <= 0);
 	}
 
-	public int GetHealth() {
+	public int GetHealth () 
+	{
 		return health;
 	}
 
-	public int GetSpeed() {
+	public int GetSpeed () 
+	{
 		return speed;
 	}
 
-	public Direction GetDirection() {
+	public Direction GetDirection () 
+	{
 		return direction;
 	}
 
-	public void UpdateHealth (int newHealth) {
+	public void UpdateHealth (int newHealth) 
+	{
 		health = newHealth;
 	}
 
-	public void UpdateSpeed (int newHealth) {
+	public void UpdateSpeed (int newHealth) 
+	{
 		health = newHealth;
 	}
 
-	public void ChangeDirection (Direction newDirection) {
+	public void ChangeDirection (Direction newDirection) 
+	{
 		direction = newDirection;
 	}
 }
