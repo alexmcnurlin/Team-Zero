11bb7f9a670157dc61b20a28a5661ccbf9ad2f0f
Zane Durkin <zanedurkin@gmail.com>
2018-02-28 20:15:50 -0800

    Adding basic movement functionality

---
+++ Assets/Scripts/zd/Testing/TestCases.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using UnityEngine;
+
+public class TestCases
+{
+	Player pancake;
+	Rigidbody2D pc_rigid;
+
+	public TestCases (Player pancake)
+	{
+		this.pancake = pancake;
+		this.pc_rigid = pancake.GetComponent<Rigidbody2D>();
+	}
+
+	public IEnumerator Wait( float seconds)
+	/* pauses for the give seconds */
+	{
+		yield return new WaitForSeconds (seconds);
+	}
+
+	/* Movement functions */
+	 
+	public IEnumerator MoveLeft(int steps=1, float force = 1.0f)
+	/* Moves Player Left */
+	{
+		Debug.Log("Moving Left");
+		for (int i = 0; i < steps; i++) {
+			yield return this.Move (- force);
+			yield return this.Wait (0.05f);
+		}
+	}
+	public IEnumerator MoveRight(int steps=1, float force = 1.0f)
+	/* Moves Player Right */
+	{
+		Debug.Log("Moving Right");
+		for (int i = 0; i < steps; i++) {
+			yield return this.Move (force);
+			yield return this.Wait (0.05f);
+		}
+	}
+	public IEnumerator Jump()
+	/* Makes Player Jump */
+	{
+		Debug.Log ("Jumping");
+		this.pc_rigid.AddForce (new Vector2 (0, 7), ForceMode2D.Impulse);
+		yield return true;
+	}
+
+
+
+	/* private Functions */
+
+	private IEnumerator Move(float force=1.0f)
+	{
+		Vector2 movement = new Vector2(pancake.local_speed.x * force, pancake.local_speed.y * 0f);
+		this.pc_rigid.AddForce (movement);
+		yield return true;
+	}
+}
---
+++ Assets/Scripts/zd/Testing/TestingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+	//public Player pancake;
+	public Player pancake;
+
+	TestCases tc; // contains common test functions
+
+	// Use this for initialization
+	void Start () {
+		this.tc = new TestCases (this.pancake);
+		StartCoroutine(RunLevel1 (true));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator RunLevel1(bool Pass=true){
+		Debug.Log ("Running Test for Level 1");
+
+		Debug.Log ("Running Right");
+		yield return this.tc.MoveRight (5, 10.0f);
+		yield return this.tc.Wait (1);
+		yield return this.tc.MoveLeft (5, 10.0f);
+		yield return this.tc.Wait (1);
+		yield return this.tc.MoveRight (5, 10.0f);
+		yield return this.tc.Wait (1);
+		yield return this.tc.Jump ();
+	}
+
+	void RunLevel2(bool Pass=true){
+	}
+		
+}
