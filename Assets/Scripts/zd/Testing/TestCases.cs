8f2294e6c0a7fbfe07612f6ef40de418fd701120
Zane Durkin <zanedurkin@gmail.com>
2018-03-05 22:52:55 -0800

    updating the auto_runner

--- Assets/Scripts/zd/Testing/TestCases.cs
+++ Assets/Scripts/zd/Testing/TestCases.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 public class TestCases
 {
 	Player pancake;
-	Rigidbody2D pc_rigid;
+	public Rigidbody2D pc_rigid;
 
 	public TestCases (Player pancake)
 	{
 		this.pancake = pancake;
 		this.pc_rigid = pancake.GetComponent<Rigidbody2D>();
 	}
+
+	/*
+	 * Public functions
+	 */
 
 	public IEnumerator Wait( float seconds)
 	/* pauses for the give seconds */
@@ -35,6 +39,22 @@ public class TestCases
 		}
 	}
 
+	public IEnumerator MoveLeftTillX(float x, float force = 10.0f, double timeout = 10.0)
+	/* Moves Player Left until it reaches the given x value */
+	{
+		//Debug.Log("Moving Left");
+		double end_time = Time.realtimeSinceStartup + timeout; // 1.0 = 1 second
+		// run till x is reached, or timeout is reached
+		while (pancake.transform.position.x < x) {
+			if (end_time < Time.realtimeSinceStartup){
+				Debug.Log ("Move Left Timeout");
+				break;
+			}
+			yield return this.Move (- force, 0.0f);
+			yield return this.Wait (0.05f);
+		}
+	}
+
 	public IEnumerator MoveRight(int steps=1, float force = 10.0f)
 	/* Moves Player Right */
 	{
@@ -44,12 +64,27 @@ public class TestCases
 			yield return this.Wait (0.05f);
 		}
 	}
-		
+
+	public IEnumerator MoveRightTillX(float x, float force = 10.0f, double timeout = 10.0)
+	/* Moves Player Right until it reaches the given x value */
+	{
+		//Debug.Log("Moving Right");
+		double end_time = Time.realtimeSinceStartup + timeout; // 1.0 = 1 second
+		// run till x is reached, or timeout is reached
+		while (pancake.transform.position.x < x) {
+			if (end_time < Time.realtimeSinceStartup){
+				Debug.Log ("Move Right Timeout");
+				break;
+			}
+			yield return this.Move (force, 0.0f);
+			yield return this.Wait (0.05f);
+		}
+	}
+
 	public IEnumerator Jump()
 	/* Makes Player Jump */
 	{
 		//Debug.Log ("Jumping");
-		//this.pc_rigid.AddForce (new Vector2 (0, 7), ForceMode2D.Impulse);
 		this.pancake.Jump();
 		yield return true;
 	}
