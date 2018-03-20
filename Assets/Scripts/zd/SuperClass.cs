4e1f12ef5477eb3c788359ee2f277d91e6dc699f
Zane Durkin <zanedurkin@gmail.com>
2018-03-20 10:26:05 -0700

    adding superclass and inheritance

---
+++ Assets/Scripts/zd/SuperClass.cs
@@ -0,0 +1,11 @@
+/*
+	Author: Zane Durkin <durk7832@vandals.uidaho.edu>
+*/
+
+using System;
+using System.Collections.Generic;
+
+abstract public class SuperClass{
+	virtual public void FakeFunc(){
+	}
+}
