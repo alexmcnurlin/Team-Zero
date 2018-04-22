61081d83aa398a4b374dfbaf7ceab75a22f52341
Westdoor6 <simonisadude@yahoo.com>
2018-04-22 15:55:51 -0700

    Added audiomanagement object into each level, singleton pattern will handle multiple cases.

--- Assets/Scripts/sb/AudioManagement.cs
+++ Assets/Scripts/sb/AudioManagement.cs
@@ -47,6 +47,8 @@ public class AudioManagement : MonoBehaviour
     //Initialize all of the sound assets
     public void Start()
     {
+
+        //All audio files are loaded from the Resources/Sound Directory
         //Music clips for background and menu
         allstar = (AudioClip)Resources.Load<AudioClip>("Sound/Allstar");
         spaceCave = (AudioClip)Resources.Load<AudioClip>("Sound/SpaceCave");
@@ -67,19 +69,26 @@ public class AudioManagement : MonoBehaviour
 
     }
 
-    public void SingletonCheck()
-    {
-        if (instance == null)
-            instance = this;
-        else if (instance != this)
-            Destroy(gameObject);
 
-        DontDestroyOnLoad(gameObject);
+    //The following function has been integrated from 
+    //https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/audio-and-sound-manager
+    void Awake()
+    {
+            //Check if there is already an instance of SoundManager
+            if (instance == null)
+                //if not, set it to this.
+                instance = this;
+            //If instance already exists:
+            else if (instance != this)
+                //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+                Destroy(gameObject);
+
+            //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+            DontDestroyOnLoad(gameObject);
     }
 
     public void Update()
     {
-        SingletonCheck();
 
         //Used to test new sounds
         if (Input.GetKeyDown("u"))
