8236bd8e9a2ebff5a3d2a3a77468ef07fae6f3c5
Jorge Olivas <japowins@gmail.com>
2018-04-30 22:19:20 -0700

    Added Player Flicker When Damaged

--- Assets/Scripts/hl/Player.cs
+++ Assets/Scripts/hl/Player.cs
@@ -31,6 +31,7 @@ abstract class PlayerAlive : IDeadPlayer
     public class Player : Character , IDeadPlayer 
     {
         private Rigidbody2D rb2d;
+        private DateTime sec;
         public int counter;
         public Vector2 localSpeed = new Vector2(10, 0);
         public Vector2 charAction;
@@ -45,8 +46,13 @@ abstract class PlayerAlive : IDeadPlayer
         public float timeLeft;
         public AudioManagement aSource;
         public int damage;
-        public int timeLength = 3;
+        private int flickerLength = 1;  //  Must be shorter than "timeLength" 
+                                        //  When changed to float - Instead of AddSeconds
+                                        //  Add MiliSeconds( flickerLength * 1000)
+        public int timeLength = 3;      //  Total Length of time it takes to "recover" from injury
         public Image damageImage;
+       
+        //public GameObject gameObject;
         public float flashSpeed = 2f;
         public Color flashColor = new Color(1f, 0f, 0f, 1f);
         private DateTime time;
@@ -75,65 +81,73 @@ abstract class PlayerAlive : IDeadPlayer
             aSource.PlayFx(AudioManagement.SoundType.JUMP);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+    // Update is called once per frame
+    void Update()
+    {
 
-            //Store the current horizontal input in the float moveHorizontal.
-            float moveHorizontal = Input.GetAxis("Horizontal");
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-            //Store the current vertical input in the float moveVertical.
-            float moveVertical = Input.GetAxis("Vertical");
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
 
-            Movement(moveHorizontal, moveVertical);
+        Movement(moveHorizontal, moveVertical);
 
-            // Prevent double jumping
-            if (Input.GetKeyDown("space") && (midJump == false))
-            {
-                Jump();
-                midJump = true;
-            }
+        // Prevent double jumping
+        if(Input.GetKeyDown("space") && (midJump == false)) {
+            Jump();
+            midJump = true;
+        }
 
-            if (GetComponent<Rigidbody2D>().velocity.y == 0)
-            {
-                midJump = false;
-            }
+        if(GetComponent<Rigidbody2D>().velocity.y == 0) {
+            midJump = false;
+        }
 
-            if (hasPowerup)
-            {
-                if (playerPowerup.IsExpired())
-                {
-                    Debug.Log("Powerup has expired");
-                    hasPowerup = false;
+        if(hasPowerup) {
+            if(playerPowerup.IsExpired()) {
+                Debug.Log("Powerup has expired");
+                hasPowerup = false;
 
-                    if (playerPowerup.type == Modifier.INVINCIBLE)
-                    {
-                        isInvincible = false;
-                    }
-
-                    else if (playerPowerup.type == Modifier.JUMPHEIGHT)
-                    {
-                        isDoubleJump = false;
-                    }
-                    else if (playerPowerup.type == Modifier.SPEED)
-                    {
-                        isFast = false;
-                    }
+                if(playerPowerup.type == Modifier.INVINCIBLE) {
+                    isInvincible = false;
+                } else if(playerPowerup.type == Modifier.JUMPHEIGHT) {
+                    isDoubleJump = false;
+                } else if(playerPowerup.type == Modifier.SPEED) {
+                    isFast = false;
                 }
-            }
-            // start recovery counter
-            TimeSpan notime = new TimeSpan(0);
-
-            if (isRecovering)
-            {
-                if (TimeLeft().CompareTo(notime) < 0)
-                {
-                    isRecovering = false;
-                }
-                damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
             }
         }
 
+        // start recovery counter
+        TimeSpan notime = new TimeSpan(0);
+        DateTime now = DateTime.Now;
+
+        if(isRecovering) {
+
+            if(sec.Subtract(now).CompareTo(notime) <= 0) {
+
+                if(gameObject.GetComponent<SpriteRenderer>().enabled) {
+                    gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                    sec = DateTime.Now.AddSeconds(flickerLength);
+                } else {
+                    gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                    sec = DateTime.Now.AddSeconds(flickerLength);
+                }
+            }
+
+            if(time.Subtract(now).CompareTo(notime) <= 0) {
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                isRecovering = false;
+            }
+
+
+            //gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+            //damageImage.GetComponent<SpriteRenderer>().color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            //damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            // }
+        }
+    }
         public void ApplyPowerup(Powerup powerup)
         {
             playerPowerup = powerup;
@@ -184,10 +198,16 @@ abstract class PlayerAlive : IDeadPlayer
             {
                 Debug.Log("Applying damage");
                 UpdateHealth(health - damage);
-                damageImage.color = flashColor;
+            //Jorge
+            //gameObject.SetActive(false);
+            //damageImage.GetComponent<SpriteRenderer>().color = Color.red;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            Debug.Log("Color is being changed to flashColor");
+                //damageImage.color = flashColor;
                 isRecovering = true;
-                time = DateTime.Now;
-                time = time.AddSeconds(length);
+                sec = DateTime.Now.AddSeconds(flickerLength);
+                Debug.Log("Timer: " + sec.Subtract(DateTime.Now).ToString());
+                time = DateTime.Now.AddSeconds(length);
             }
         }
 
